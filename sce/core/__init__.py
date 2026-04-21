144bca6a1202226dc930a3ee8449be436146768b
yanixkz <yanixkz@gmail.com>
2026-04-21 15:52:06 +0500

    Export episode repository interfaces from sce.core

--- sce/core/__init__.py
+++ sce/core/__init__.py
@@ -16,3 +16,4 @@ from sce.core.scoring import SCEScoringEngine
 from sce.core.evolution import SCEEvolver
 from sce.core.explain import SCEExplainer
 from sce.core.constraint_dsl import compile_constraint_dsl
+from sce.core.memory_repository import EpisodeRepository, InMemoryEpisodeRepository
